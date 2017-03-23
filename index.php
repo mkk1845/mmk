c47d14a76b522473bbb4dc6b308a07de91fe7e30
colorful <704826318@qq.com>
2017-03-23 20:11:31 +0800

    首页

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<?php
+if(isset($_POST["sub"]))
+{
+	echo $_POST["us"];
+}
+
+?>
+<form method="post" action="">
+<input type="text" name="us" >
+<input type="submit" name="sub" value="µã»÷"></form>
+</body>
+</html>
